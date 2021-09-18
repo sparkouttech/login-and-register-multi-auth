f13a1896f724eb2460a76a6e805a78a1e068d882
muruganSparkout <murugan@sparkouttech.com>
2021-09-18 16:20:24 +0530

    add config files

--- src/config/user-auth.php
+++ src/config/user-auth.php
@@ -6,10 +6,23 @@
 return [
 
     "login_redirect" => "/",
-    
+
     // Enable / Disable referral module
     "referral" => true,
     "referral_code_length" => 10,
+    /*
+     * Phone Status
+     * email,phone,username
+     */
+    "login_type" => email,
+    /*
+     * Phone Status
+     */
+    'phone_status' => false,
+    /*
+    * Registration Verification Status
+    */
+    'register_verification' => false,
 
     // crypto configuration files
     "crypto" => [
